485fae77dcbaaa41cfa8306b08cbd349b1afa206
Daniel Diaz <46817561+daniel-diaz-ucl@users.noreply.github.com>
2024-08-30 13:33:12 +0100

    Changes to be committed: 	modified:   notebooks/exploratory/hpc_cluster_scripts/bert_large_training_THSK2023.py 	modified:   notebooks/exploratory/hpc_cluster_scripts/bert_training_THSK2023.py 	modified:   notebooks/exploratory/hpc_cluster_scripts/roberta_large_training_THSK2023.py 	modified:   notebooks/exploratory/hpc_cluster_scripts/roberta_training_THSK2023.py 	modified:   notebooks/exploratory/hpc_cluster_scripts/test_bert_training_THSK2023.py

--- notebooks/exploratory/hpc_cluster_scripts/bert_training_THSK2023.py
+++ notebooks/exploratory/hpc_cluster_scripts/bert_training_THSK2023.py
@@ -139,18 +139,21 @@ def compute_metrics(p):
     preds = np.argmax(p.predictions, axis=1)
     precision, recall, f1, _ = precision_recall_fscore_support(p.label_ids, preds, average='binary')
     acc = accuracy_score(p.label_ids, preds)
+    loss = p.loss
     metrics.append({
         'epoch': trainer.state.epoch,
         'accuracy': acc,
         'precision': precision,
         'recall': recall,
         'f1': f1,
+        'loss': loss,
     })
     return {
         'accuracy': acc,
         'precision': precision,
         'recall': recall,
         'f1': f1,
+        'loss': loss,
     }
 
 # Trainer
